35ef15a8d9b1aac290de38df31c3a1ac4701893d
soCromp <snc40@pitt.edu>
2021-12-21 00:15:30 -0600

    dataset, model and util files

---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import
+from .anom_utils import *
+from .common import *
+from .svhn_loader import *
---
+++ utils/common.py
@@ -0,0 +1,63 @@
+import os
+import torch
+
+class UnNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized image.
+        """
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.mul_(s).add_(m)
+        return tensor
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += self.val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+
+def save_checkpoint(args, state, epoch, name = None):
+    """Saves checkpoint to disk"""
+    directory = "experiments/{in_dataset}/{name}/checkpoints/".format(in_dataset=args.in_dataset, name=args.name)
+    os.makedirs(directory, exist_ok=True)
+    if name == None:
+        filename = directory + 'checkpoint_{}.pth.tar'.format(epoch)
+    else: 
+        filename = directory + 'checkpoint_{}.pth.tar'.format(name)
+    torch.save(state, filename)
